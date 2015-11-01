7ef2ee571116ccf1dafe62a99ff7a9a94ffd6b9e
Timothé Mermet-Buffet <mermetbt@gmail.com>
2015-11-01 17:26:34 +0800

    Change Symfony console version.

--- src/Biome/Core/Command/AbstractCommand.php
+++ src/Biome/Core/Command/AbstractCommand.php
@@ -2,8 +2,12 @@
 
 namespace Biome\Core\Command;
 
-use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
 
 abstract class AbstractCommand extends Command
 {
@@ -22,10 +26,38 @@ abstract class AbstractCommand extends Command
 			$group_name = strtolower(substr($class, 0, -strlen('Command')));
 			$command_name = $method->getName();
 
+			/**
+			 * Parameters handling.
+			 */
+			$parameters = array();
+			foreach($method->getParameters() AS $param)
+			{
+				$name = $param->getName();
+
+				// PHP 7
+				if(method_exists($param, 'getType'))
+				{
+					$type = $param->getType();
+					$required = !$param->allowsNull();
+				}
+				// PHP 5
+				else
+				{
+					$matches = array();
+					preg_match('/\[(.*)\]/', $param_str, $matches);
+
+					$raw = explode(' ', trim($matches[1]));
+
+					$required = ($raw[0] == '<required>') ? TRUE : FALSE;
+					$type = ($raw[1][0] == '$') ? '' : $raw[1];
+				}
+
+				$required_constant = $required ? InputArgument::REQUIRED : '';
+				$parameters[] = new InputArgument($name, $required_constant, 'Directory name');
+			}
+
 			$console->register($group_name . ':' . $command_name)
-					->setDefinition(array(
-						new InputArgument('dir', InputArgument::REQUIRED, 'Directory name'),
-					))
+					->setDefinition($parameters)
 					->setDescription('Displays the files in the given directory')
 					->setCode(function (InputInterface $input, OutputInterface $output) {
 						$dir = $input->getArgument('dir');
