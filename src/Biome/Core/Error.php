42bd3478c43d2ce83e8b99380431458f7ffc687a
Timothé Mermet-Buffet <mermetbt@gmail.com>
2016-08-05 22:05:45 +0200

    Allow disabling Whoops.

--- src/Biome/Core/Error.php
+++ src/Biome/Core/Error.php
@@ -7,25 +7,28 @@ class Error
 	public static function init()
 	{
 		error_reporting(E_ALL);
-		$whoops = new \Whoops\Run;
 
-		$request = \Biome\Biome::getService('request');
-		if($request->acceptHtml())
+		if(Config\Config::get('WHOOPS_ERROR', TRUE))
 		{
-			$whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
-		}
-		else
-		if(\Whoops\Util\Misc::isCommandLine())
-		{
-			$whoops->pushHandler(new \Whoops\Handler\PlainTextHandler);
-		}
-		else
-		{
-			$whoops->pushHandler(new \Whoops\Handler\JsonResponseHandler);
-		}
+			$whoops = new \Whoops\Run;
 
-		$whoops->register();
+			$request = \Biome\Biome::getService('request');
+			if($request->acceptHtml())
+			{
+				$whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
+			}
+			else
+			if(\Whoops\Util\Misc::isCommandLine())
+			{
+				$whoops->pushHandler(new \Whoops\Handler\PlainTextHandler);
+			}
+			else
+			{
+				$whoops->pushHandler(new \Whoops\Handler\JsonResponseHandler);
+			}
+
+			$whoops->register();
+		}
 	}
 
 }
-
