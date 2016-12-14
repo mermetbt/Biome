fd75c426961f54d72e3d191f81976aee7f3f3fd0
Timothé Mermet-Buffet <tim@aliengen.com>
2016-12-14 13:28:37 +0800

    Not force whoops on debug.

--- src/Biome/Core/Error.php
+++ src/Biome/Core/Error.php
@@ -8,7 +8,7 @@ class Error
 	{
 		error_reporting(E_ALL);
 
-		if(Config\Config::get('WHOOPS_ERROR', FALSE) || Config\Config::get('DEBUG', FALSE))
+		if(Config\Config::get('WHOOPS_ERROR', TRUE) && Config\Config::get('DEBUG', FALSE))
 		{
 			$whoops = new \Whoops\Run;
 
